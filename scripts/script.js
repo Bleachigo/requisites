cc0c85913535d5f5d4841bde96d2a8512d026444
Artem <artwhite2012@gmail.com>
2026-02-06 19:23:26 +0200

    Producing second section (after payment)

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,7 @@
-// const copyIbanBtn = document.getElementById("copy-iban");
-// const copyEdrpouBtn = document.getElementById("copy-edrpou");
-// const copyPurposeBtn = document.getElementById("copy-purpose");
-//
-// const ibanNumber = document.getElementById("iban");
-// const edrpouNumber = document.getElementById("edrpou");
-// const paymentPurpose = document.getElementById("payment-purpose");
-
 const copyButtons = document.querySelectorAll(".btn");
 
 copyButtons.forEach((button) => {
   button.addEventListener("click", async () => {
-    // const ibanToCopy = iban.innerText;
     // Find the specific paragraph linked via data-target
     const targetId = button.getAttribute("data-target");
     const textToCopy = document.getElementById(targetId).innerText;
